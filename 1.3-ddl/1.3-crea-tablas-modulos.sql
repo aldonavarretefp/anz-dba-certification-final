5e4eb8b33e96946c24727cfdad4aaf57feb024df
Aldo Navarrete <aaldiitoo@gmail.com>
2025-05-30 14:31:25 -0600

    Refactor SQL scripts for improved readability and structure; added formatting for output columns, adjusted container settings, and modified table definitions.

--- 1.3-ddl/1.3-crea-tablas-modulos.sql
+++ 1.3-ddl/1.3-crea-tablas-modulos.sql
@@ -7,12 +7,12 @@ set serveroutput on
 
 Prompt "1.3. Creando usuarios para el modulo 1..."
 alter session set container = tvac_modulo_1;
-connect usermod1/usermod1@tvac_modulo_1;
+connect tvac_mod1_app/tvac_mod1_app@TVAC_MOD1;
 @1.3.1-modulo-1-ddl.sql
 
 Prompt "1.3. Creando usuarios para el modulo 2..."
 alter session set container = tvac_modulo_2;
-connect usermod2/usermod2@tvac_modulo_2;
+connect tvac_mod2_app/tvac_mod2_app@TVAC_MOD2;
 @1.3.2-modulo-2-ddl.sql
 
 spool off
